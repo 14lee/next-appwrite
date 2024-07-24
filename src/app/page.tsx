7f63ba3c0b16bc2e8e7a0e0928aed44b5d25e927
lyd <liyudong@southzn.com>
2024-07-24 13:48:43 +0800

    Feat: :tada: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import { useEffect, useState } from 'react'
+import { Client } from 'appwrite';
 
 export default function Home() {
+  useEffect(() => {
+    const client = new Client();
+
+    client
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject('66a076f700346292a010');
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
